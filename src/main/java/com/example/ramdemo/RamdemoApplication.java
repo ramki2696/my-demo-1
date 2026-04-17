0f3daf4e0abb754070266e6fbc61ff2c623e2228
ramki <ramki2696@gmail.com>
2026-04-17 09:49:26 +0530

    main

---
+++ src/main/java/com/example/ramdemo/RamdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ramdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ramdemo/RamdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ramdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RamdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
